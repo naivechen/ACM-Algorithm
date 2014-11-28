3406abb5fdcebab42790f732e61cbf08d03a6fed
Mingming Chen <naivechen02@gmail.com>
2014-11-28 22:01:41 +0800

    recursion

---
+++ Others/汉诺塔游戏.cpp
@@ -0,0 +1,49 @@
+// http://codevs.cn/problem/3145/
+
+
+#pragma comment(liner, "/STACK:1024000000,1024000000")
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#define LL long long int
+#define L(u) (u<<1)
+#define R(u) (u<<1|1)
+#define pii pair<int,int>
+#define mpair make_pair
+const int maxn=112010;
+const double eps=1e-8;
+const int fdir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+const int edir[8][2]={{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+const int mod=1000000007;
+const int inf=0x3fffffff;
+const double pi=acos(-1.0);
+typedef vector<int> INT;
+//---------------------------------------------------------
+int n;
+void solve(int a,char q,char z,char m)
+{
+    if(a!=1) solve(a-1,q,m,z);
+    cout<<a<<" from "<<q<<" to "<<m<<endl;
+    if(a!=1) solve(a-1,z,q,m);
+}
+int main()
+{
+    cin>>n;
+    cout<<pow(2.0,n)-1<<endl;
+    solve(n,'A','B','C');
+}
