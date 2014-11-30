301c11151ace9c827e521ce73488512238ddd8f8
Mingming Chen <naivechen02@gmail.com>
2014-11-30 22:38:04 +0800

    CODEVS 1007 (easy)

---
+++ Math/级数求和.cpp
@@ -0,0 +1,49 @@
+// http://codevs.cn/problem/1007/
+
+
+#pragma comment(liner, "/STACK:1024000000,1024000000")
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#define LL long long int
+#define L(u) (u<<1)
+#define R(u) (u<<1|1)
+#define pii pair<int,int>
+#define mpair make_pair
+const int maxn=210;
+const double eps=1e-8;
+const int fdir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+const int edir[8][2]={{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+const int mod=1000000007;
+const int inf=0x3fffffff;
+const double pi=acos(-1.0);
+//---------------------------------------------------------
+int main()
+{
+    double k;
+    double ans=0;
+    cin>>k;
+    double i=1.0;
+    while(ans-k<=eps)
+    {
+        double tmp=1.0/i;
+        ans+=tmp;
+        i=i+1.0;
+    }
+    cout<<(int)i-1<<endl;
+}
