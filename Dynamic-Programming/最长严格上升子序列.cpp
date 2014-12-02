902360890ac58103c5d4680936779f0b168594a6
Mingming Chen <naivechen02@gmail.com>
2014-12-02 17:58:51 +0800

    CODEVS 1576 (easy dp)

---
+++ Dynamic-Programming/最长严格上升子序列.cpp
@@ -0,0 +1,47 @@
+// http://codevs.cn/problem/1576/
+
+
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <cassert>
+#include <set>
+using namespace std;
+#define LL long long
+#define L(u) (u<<1)
+#define R(u) (u<<1|1)
+#define pii pair<int,int>
+#define mpair make_pair
+const int maxn=5300;
+const double eps=1e-8;
+const int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+const int mod=1000000007;
+const int inf=0x3fffffff;
+const double pi=acos(-1.0);
+//---------------------------------------------------------------
+int n,a[maxn],dp[maxn];
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        cin>>a[i];
+    for(int i=1;i<=n;i++)
+        dp[i]=1;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<i;j++)
+        {
+            if(a[j]<a[i]) dp[i]=max(dp[i],dp[j]+1);
+        }
+    cout<<dp[n]<<endl;
+}
+
