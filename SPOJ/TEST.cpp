78fe87df70a1495a72ace18e6a5bc7321bb67c79
Mingming Chen <naivechen02@gmail.com>
2014-12-06 10:58:46 +0800

    easy

---
+++ SPOJ/TEST.cpp
@@ -0,0 +1,39 @@
+// http://www.spoj.com/problems/TEST/
+
+
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <cassert>
+#include <set>
+using namespace std;
+#define LL long long
+#define L(u) (u<<1)
+#define R(u) (u<<1|1)
+#define pii pair<int,int>
+#define mpair make_pair
+const int maxn=210;
+const double eps=1e-8;
+const int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+const int mod=1000000007;
+const int inf=0x3fffffff;
+const double pi=acos(-1.0);
+//---------------------------------------------------------------
+int main()
+{
+    int val;
+    while(cin>>val && val!=42)
+    {
+        cout<<val<<endl;
+    }
+}
