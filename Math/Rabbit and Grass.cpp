59711bbe94235c0ad37d71ccc4afd395f0c130f8
Mingming Chen <naivechen02@gmail.com>
2015-03-09 23:55:10 +0800

    game theory

---
+++ Math/Rabbit and Grass.cpp
@@ -0,0 +1,60 @@
+// http://acm.hdu.edu.cn/showproblem.php?pid=1849
+
+// When I wrote this, only God and I understood what I was doing
+// now, God only knows
+
+#pragma comment(liner, "/STACK:1024000000,1024000000")
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+#define LL long long int
+#define L(u) (u<<1)
+#define R(u) (u<<1|1)
+#define pii pair<int,int>
+#define mpair make_pair
+const int maxn=100010;
+const double eps=1e-8;
+const int fdir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+const int edir[8][2]={{1,0},{-1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+const int hdir[8][2]={{-1,-2},{-1,2},{1,-2},{1,2},{-2,1},{2,1},{-2,-1},{2,-1}};
+const int mod=1000000007;
+const int inf=0x3fffffff;
+const double pi=acos(-1.0);
+int read()
+{
+    int x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
+    return x*f;
+}
+//---------------------------------------------------------
+int n,m;
+int main()
+{
+    while(cin>>n && n)
+    {
+        int tmp=0;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>m;
+            tmp^=m;
+        }
+        if(tmp) cout<<"Rabbit Win!"<<endl;
+        else cout<<"Grass Win!"<<endl;
+    }
+}
